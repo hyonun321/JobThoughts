1727dfbcda48ecedc19eb80a5ff361c6c3c8d804
sejinbaek <sejin9767@gmail.com>
2025-05-13 22:06:49 +0900

    Feat: 기본 Button 컴포넌트 생성 및 스타일링 구현
    - text, color, size, background, border 등 props 기반 커스텀 가능
    - hover, disabled 상태 스타일 적용
    - theme.colors 및 theme.fontSize 연동
    - 재사용 가능한 기본 버튼 컴포넌트 구현

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,3 +1,96 @@
-export default function Button() {
-  return <>버튼</>;
+import styled from 'styled-components';
+import { theme } from '../styles/theme';
+
+type ButtonProps = {
+  text: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  color?: keyof typeof theme.colors | string;
+  size?: keyof typeof theme.fontSize | string;
+  padding?: string;
+  transition?: string;
+  hoverColor?: keyof typeof theme.colors | string;
+  backgroundColor?: keyof typeof theme.colors | string;
+  border?: string;
+  boxShadow?: string;
+};
+
+const ButtonStyle = styled.button<{
+  color?: keyof typeof theme.colors | string;
+  size?: keyof typeof theme.fontSize | string;
+  padding?: string;
+  transition?: string;
+  hoverColor?: keyof typeof theme.colors | string;
+  backgroundColor?: keyof typeof theme.colors | string;
+  border?: string;
+  boxShadow?: string;
+}>`
+  padding: ${({ padding }) => padding || '16px 24px'};
+  background-color: ${({ theme, backgroundColor }) =>
+    backgroundColor ? backgroundColor : theme.colors.primary};
+
+  font-size: ${({ theme, size }) => theme.fontSize[size as keyof typeof theme.fontSize] || size};
+
+  color: ${({ theme, color }) => theme.colors[color as keyof typeof theme.colors] || 'white'};
+
+  ${({ border }) =>
+    border &&
+    `
+  border: ${border};
+`}
+  border-radius:100px;
+
+  ${({ boxShadow }) =>
+    boxShadow &&
+    `
+  box-shadow: ${boxShadow};
+`}
+
+  ${({ transition }) => transition && `transition: ${transition};`}
+
+${({ theme, hoverColor }) =>
+    hoverColor &&
+    `
+  &:hover {
+    background-color: ${theme.colors[hoverColor as keyof typeof theme.colors] || hoverColor};
+    color: white;
+  }
+`}
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.gray400};
+    color: white;
+    cursor: not-allowed;
+    box-shadow: none;
+  }
+`;
+
+export default function Button({
+  text,
+  onClick,
+  disabled = false,
+  color = 'white',
+  size = '16px',
+  padding = '16px 24px',
+  transition,
+  hoverColor,
+  backgroundColor = 'primary',
+  border,
+  boxShadow,
+}: ButtonProps) {
+  return (
+    <ButtonStyle
+      onClick={onClick}
+      disabled={disabled}
+      color={color}
+      size={size}
+      padding={padding}
+      transition={transition}
+      hoverColor={hoverColor}
+      backgroundColor={backgroundColor}
+      border={border}
+      boxShadow={boxShadow}
+    >
+      {text}
+    </ButtonStyle>
+  );
 }
