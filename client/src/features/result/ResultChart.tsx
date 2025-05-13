e277d335bf324aee6042a1c30228aff0664a155c
sejinbaek <sejin9767@gmail.com>
2025-05-13 12:52:52 +0900

    Feat: nivo radar 차트 초기 렌더링 구현

--- client/src/features/result/ResultChart.tsx
+++ client/src/features/result/ResultChart.tsx
@@ -1,7 +1,39 @@
-export default function ResultChart() {
+import { ResponsiveRadar } from '@nivo/radar';
+import resultData from '../../data/resultData';
+import styled from 'styled-components';
+
+type ChartContainerProps = {
+  size?: number;
+};
+
+const ChartContainer = styled.div<ChartContainerProps>`
+  width: ${({ size }) => size || 500}px;
+  height: ${({ size }) => size || 500}px;
+`;
+
+export default function ResultChart({ size = 500 }: ChartContainerProps) {
   return (
-    <div>
-      <img src="images/chart-placeholder.png" alt="가치관 레이더 차트" style={{ width: '100%' }} />
-    </div>
+    <ChartContainer>
+      <ResponsiveRadar
+        data={resultData}
+        keys={['score']}
+        indexBy="type"
+        maxValue="auto"
+        margin={{ top: 50, right: 80, bottom: 50, left: 150 }}
+        curve="linearClosed"
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [['darker', 1]] }}
+        gridLevels={5}
+        gridShape="circular"
+        gridLabelOffset={36}
+        enableDots={false}
+        colors={['#4F63FF']}
+        fillOpacity={0.4}
+        blendMode="normal"
+        animate={true}
+        motionConfig="gentle"
+        isInteractive={true}
+      />
+    </ChartContainer>
   );
 }
