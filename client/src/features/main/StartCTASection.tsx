cc328e67b8ee33720d0f90a92189aa4d82b10ae1
sejinbaek <sejin9767@gmail.com>
2025-05-13 22:18:35 +0900

    Feat: 메인 페이지 지금 시작하기에 버튼 컴포넌트 적용

--- client/src/features/main/StartCTASection.tsx
+++ client/src/features/main/StartCTASection.tsx
@@ -1,11 +1,22 @@
 import { useNavigate } from 'react-router-dom';
 import FullScreenSection from '../../components/FullScreenSection';
+import Button from '../../components/Button';
 
 export default function StartCTASection() {
   const navigate = useNavigate();
   return (
     <FullScreenSection>
-      <button onClick={() => navigate('/test')}>지금 시작하기</button>
+      <Button
+        onClick={() => navigate('/test')}
+        text={'지금 시작하기'}
+        padding={'20px 72px'}
+        size="lg"
+        color={'primary'}
+        transition={'all 0.3s ease'}
+        backgroundColor={'white'}
+        hoverColor={'primary'}
+        boxShadow={'0px 0px 10px rgba(79, 99, 255, 0.4)'}
+      />
     </FullScreenSection>
   );
 }
