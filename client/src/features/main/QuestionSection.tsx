f12c8ad3a18d25e929bbb1065bf68a0dd056d4ce
sejinbaek <sejin9767@gmail.com>
2025-05-14 19:27:17 +0900

    Chore: 불필요한 코드 제거

--- client/src/features/main/QuestionSection.tsx
+++ client/src/features/main/QuestionSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { motion, useInView, useAnimation } from 'framer-motion';
 import type { Variants } from 'framer-motion';
