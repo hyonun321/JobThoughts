836f83287531d650eb7af2188c7edb079df4dba5
sejinbaek <sejin9767@gmail.com>
2025-05-15 00:24:37 +0900

    Style: ResultPage 윗부분 레이아웃 스타일 적용

--- client/src/pages/ResultPage.tsx
+++ client/src/pages/ResultPage.tsx
@@ -1,13 +1,32 @@
 import ResultChart from '../features/result/ResultChart';
 import ResultDescriptionCard from '../features/result/ResultDescriptionCard';
 import JobGroupSection from '../features/result/JobGroupSection';
+import styled from 'styled-components';
+
+const ResultSection = styled.div`
+  padding: 20px 20px;
+`;
+
+const ResultTopWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem;
+  justify-content: center;
+  align-items: center;
+  > * {
+    flex: 1 1 300px; /* 최소 300px까지 줄어듦 */
+    max-width: 600px; /* (선택) 너무 커지지 않게 제한 */
+  }
+`;
 
 export default function ResultPage() {
   return (
-    <>
-      <ResultChart />
-      <ResultDescriptionCard />
+    <ResultSection>
+      <ResultTopWrapper>
+        <ResultChart />
+        <ResultDescriptionCard />
+      </ResultTopWrapper>
       <JobGroupSection />
-    </>
+    </ResultSection>
   );
 }
