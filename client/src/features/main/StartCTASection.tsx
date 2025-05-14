89c1623811f24fd59840ba15fea9a22597bf884d
H-JuKyung <hjk000hjk17@gmail.com>
2025-05-14 10:57:39 +0900

    Feat: StartCTASection에 배경 그라데이션 스타일 적용

--- client/src/features/main/StartCTASection.tsx
+++ client/src/features/main/StartCTASection.tsx
@@ -1,11 +1,18 @@
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import FullScreenSection from '../../components/FullScreenSection';
 import Button from '../../components/Button';
+
+const CTAWrapper = styled(FullScreenSection)`
+  background: linear-gradient(to bottom, #ffffff 0%, #4f63ff 52%, #000000 97%);
+  position: relative;
+  overflow: hidden;
+`;
 
 export default function StartCTASection() {
   const navigate = useNavigate();
   return (
-    <FullScreenSection>
+    <CTAWrapper>
       <Button
         onClick={() => navigate('/test')}
         text={'지금 시작하기'}
@@ -17,6 +24,6 @@ export default function StartCTASection() {
         hoverColor={'primary'}
         boxShadow={'0px 0px 10px rgba(79, 99, 255, 0.4)'}
       />
-    </FullScreenSection>
+    </CTAWrapper>
   );
 }
