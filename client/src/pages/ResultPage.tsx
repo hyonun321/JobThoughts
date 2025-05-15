75570b49536872443ab3ea6abbd50424d3ba1ed4
sejinbaek <sejin9767@gmail.com>
2025-05-16 01:47:06 +0900

    Feat: 직업 검사 결과 페이지 최상단 차트 라벨 사용법 문구 추가
    - 사용자가 쉽게 접근할 수 있도록 차트 라벨 사용 방법 문구를 추가하였습니다.
    - 통일성 있는 스타일을 위해 theme.js을 적용하였습니다.

--- client/src/pages/ResultPage.tsx
+++ client/src/pages/ResultPage.tsx
@@ -4,9 +4,38 @@ import JobGroupSection from '../features/result/JobGroupSection';
 import styled from 'styled-components';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
+import ClickFinger from '../../src/assets/icons/icon-click-finger.svg';
+import { theme } from '../../src/styles/theme';
+
+const LayoutTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+
+  h1 {
+    text-align: center;
+    color: ${theme.colors.gray900};
+    font-size: ${theme.fontSize.xl};
+    font-weight: normal;
+    transform: translateX(30px);
+  }
+
+  span {
+    color: ${theme.colors.primary};
+  }
+
+  .image-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transform: translate(-32px, -22px); /* 이미지만 살짝 왼쪽/위로 */
+  }
+`;
 
 const ResultSection = styled.div`
   padding: 0px 20px;
+  margin-top: 30px;
   display: flex;
   flex-direction: column;
 `;
@@ -17,6 +46,7 @@ const ResultTopWrapper = styled.div`
   gap: 3rem;
   justify-content: center;
   align-items: center;
+  margin-top: -70px;
   > * {
     flex: 1 1 300px; /* 최소 300px까지 줄어듦 */
     max-width: 600px; /* (선택) 너무 커지지 않게 제한 */
@@ -49,6 +79,14 @@ export default function ResultPage() {
   const [selectedLabel, setSelectedLabel] = useState<string | null>(null);
   return (
     <ResultSection>
+      <LayoutTitle>
+        <h1>
+          차트의 각 항목을 <span>클릭</span> 해보세요!
+          <br />
+          나의 직업 가치관에 대한 설명을 확인할 수 있어요
+        </h1>
+        <img className="image-wrapper" src={ClickFinger} alt="클릭하는 손가락 아이콘" />
+      </LayoutTitle>
       <ResultTopWrapper>
         <motion.div key="chart" layout transition={layoutSpring}>
           <ResultChart
