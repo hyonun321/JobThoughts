a9addb09e16cc4c427e270ce72d87ec56dbbf0fb
sejinbaek <sejin9767@gmail.com>
2025-05-15 16:50:46 +0900

    Feat: 직업 리스트 초기 레이아웃 구성
    - 직업 추천 섹션 기본 레이아웃 구현 (Section, Group, Category, JobList 등)
    - 가치관 소개 텍스트와 이미지 레이아웃(JobInfoText) 추가
    - theme 색상, 폰트 사용으로 통일된 스타일 적용

--- client/src/features/result/JobGroupSection.tsx
+++ client/src/features/result/JobGroupSection.tsx
@@ -2,19 +2,63 @@ import styled from 'styled-components';
 import jobRecommendationData from '../../data/jobRecommendationData';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
+import { theme } from '../../styles/theme';
 
+// 전체 레이아웃
 const Section = styled.div`
-  margin-top: 32px;
+  border: 1px solid green;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 50px;
 `;
 
+const JobInfoText = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 20vw;
+  }
+`;
+
+const SmallTitle = styled.span`
+  background-color: #f5f9ff;
+  color: ${theme.colors.primary};
+  padding: 16px 24px;
+  border-radius: 100px;
+  box-shadow: 2px 3px 1px rgb(200, 224, 255, 1);
+  font-size: ${theme.fontSize.ml};
+  font-weight: ${theme.fontWeight.medium};
+`;
+
+const Highlight = styled.span`
+  color: ${theme.colors.primary};
+`;
+
+// 계열별 직업 리스트 레이아웃
 const Group = styled.div`
+  border: 1px solid blue;
+  display: flex;
+  align-items: center;
+  gap: 8px;
   margin-bottom: 24px;
 `;
 
+// 계열 레이아웃
+const Category = styled.div`
+  border: 1px solid green;
+  padding: 0 20px;
+  width: 150px;
+  text-align: center;
+`;
+
+// 직업 버튼 레이아웃
 const JobList = styled.div`
+  border: 1px solid red;
   display: flex;
   flex-wrap: wrap;
   gap: 8px;
+  flex: 1;
 `;
 
 export default function JobGroupSection() {
@@ -26,12 +70,24 @@ export default function JobGroupSection() {
 
   return (
     <Section>
-      <h2>이런 직업은 어때요?</h2>
-      {jobRecommendationData.map(({ category, jobs, emoji }) => (
+      <JobInfoText>
+        <div>
+          <SmallTitle>나의 가치관과 관련이 높은 직업</SmallTitle>
+          <h2>
+            <Highlight>자율성</Highlight>도 챙기고, <Highlight>보수</Highlight>도 놓치기 싫은 당신!
+            이런 직업은 어때요?
+          </h2>
+          <h3>직업을 클릭하면, 실시간 채용 공고까지 확인할 수 있어요.</h3>
+        </div>
+        <div>
+          <img src="" alt="직업을 소개하는 곰돌이" />
+        </div>
+      </JobInfoText>
+      {jobRecommendationData.map(({ category, jobs }) => (
         <Group key={category}>
-          <h4>
-            {emoji} {category}
-          </h4>
+          <Category>
+            <h4>{category}</h4>
+          </Category>
           <JobList>
             {jobs.map((job) => (
               <Button
