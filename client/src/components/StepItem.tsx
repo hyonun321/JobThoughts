2ce4cf86d7ea53ad815f9f1cb8ed3ae2aeb4ae57
hyonun321 <hyo_nun@kakao.com>
2025-05-15 14:29:34 +0900

    Feat: active 속성에 따라 투명도 및 색상 변하도록 설정

--- client/src/components/StepItem.tsx
+++ client/src/components/StepItem.tsx
@@ -1,5 +1,3 @@
-import { useRef } from 'react';
-import { useInView } from 'framer-motion';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -11,15 +9,15 @@ const Container = styled.div`
 const Number = styled.div<{ active: boolean }>`
   font-size: 2.5rem;
   font-weight: 700;
-  display: flex;
   color: ${({ active }) => (active ? '#5668FF' : '#A0A3FF')};
   margin-right: 1rem;
   transition: color 0.3s;
 `;
 
 const Label = styled.div<{ active: boolean }>`
-  background-color: ${({ active }) => (active ? '#5668FF' : '#D6DBFF')};
-  color: ${({ active }) => (active ? '#ffffff' : 'rgba(255,255,255,0.7)')};
+  background-color: ${({ active, theme }) =>
+    active ? theme.colors.primary : theme.colors.background};
+  color: ${({ active, theme }) => (active ? theme.colors.white : 'rgba(255,255,255,0.7)')};
   margin-left: 3rem;
   border-radius: 18px;
   padding: 1rem 2rem;
@@ -36,13 +34,19 @@ const Label = styled.div<{ active: boolean }>`
     opacity 0.3s;
 `;
 
-export default function StepItem({ step, text }: { step: number; text: string }) {
-  const ref = useRef<HTMLDivElement>(null);
-  const inView = useInView(ref, { threshold: 0.5 });
+export default function StepItem({
+  step,
+  text,
+  active,
+}: {
+  step: number;
+  text: string;
+  active: boolean;
+}) {
   return (
-    <Container ref={ref}>
-      <Number active={inView}>{`Step ${step}`}</Number>
-      <Label active={inView}>{text}</Label>
+    <Container>
+      <Number active={active}>{`Step ${step}`}</Number>
+      <Label active={active}>{text}</Label>
     </Container>
   );
 }
