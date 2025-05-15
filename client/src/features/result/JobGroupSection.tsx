66f8de7fef49f1ec17151a1ea77625d8e0a85648
sejinbaek <sejin9767@gmail.com>
2025-05-15 17:09:13 +0900

    Feat: 계열별 직업 리스트에 곰돌이 이미지 렌더링
    - 계열별 직업 리스트에 들어갈 곰돌이 이미지를 객체로 저장
    - map을 통해 계열에 맞는 이미지 렌더링

--- client/src/features/result/JobGroupSection.tsx
+++ client/src/features/result/JobGroupSection.tsx
@@ -4,6 +4,26 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import { theme } from '../../styles/theme';
 
+// 곰돌이 이미지
+import JobIntroBear from '../../assets/bears/job-introduce-bear.png';
+import WorkBear from '../../assets/bears/bears-1.svg';
+import LookingBear from '../../assets/bears/bears-2.svg';
+import Bookbear from '../../assets/bears/bears-3.svg';
+import MedicalBear from '../../assets/bears/bears-4.svg';
+import NatureBear from '../../assets/bears/bears-5.svg';
+import EngineerBear from '../../assets/bears/bears-6.png';
+import ArtBear from '../../assets/bears/bears-7.png';
+
+const categoryImages: Record<string, string> = {
+  계열무관: LookingBear,
+  인문: Bookbear,
+  사회: WorkBear,
+  공학: EngineerBear,
+  의학: MedicalBear,
+  자연: NatureBear,
+  예체능: ArtBear,
+};
+
 // 전체 레이아웃
 const Section = styled.div`
   border: 1px solid green;
@@ -40,16 +60,28 @@ const Group = styled.div`
   border: 1px solid blue;
   display: flex;
   align-items: center;
-  gap: 8px;
+  gap: 15px;
   margin-bottom: 24px;
 `;
 
 // 계열 레이아웃
 const Category = styled.div`
-  border: 1px solid green;
-  padding: 0 20px;
-  width: 150px;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  width: 250px;
+  flex-shrink: 0;
+
+  img {
+    width: 150px;
+    height: 170px;
+    object-fit: contain;
+  }
+
+  h4 {
+    font-size: 22px;
+    margin: 0;
+    white-space: nowrap; /* 줄바꿈 방지 */
+  }
 `;
 
 // 직업 버튼 레이아웃
@@ -80,12 +112,15 @@ export default function JobGroupSection() {
           <h3>직업을 클릭하면, 실시간 채용 공고까지 확인할 수 있어요.</h3>
         </div>
         <div>
-          <img src="" alt="직업을 소개하는 곰돌이" />
+          <img src={JobIntroBear} alt="직업을 소개하는 곰돌이" />
         </div>
       </JobInfoText>
       {jobRecommendationData.map(({ category, jobs }) => (
         <Group key={category}>
           <Category>
+            <div>
+              <img src={categoryImages[category]} alt={category} />
+            </div>
             <h4>{category}</h4>
           </Category>
           <JobList>
