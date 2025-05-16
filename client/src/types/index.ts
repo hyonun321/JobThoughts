0734fe537cd97f404cd87926a7e3190ebad7946e
hyonun321 <hyo_nun@kakao.com>
2025-05-16 11:51:34 +0900

    Feat: Job Type 선언

---
+++ client/src/types/index.ts
@@ -0,0 +1,14 @@
+export type Job = {
+  id: number;
+  job: string;
+  company: string;
+  title: string;
+  career: string;
+  education: string;
+  salary: string;
+  workDays: string;
+  location: string;
+  type: string;
+  postedAt: string;
+  link: string;
+};
