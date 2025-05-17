c0f08e48536461ea43caa38064c421e92a096c5c
sejinbaek <sejin9767@gmail.com>
2025-05-17 10:42:32 +0900

    Chore: dots, sentences 함수의 명시적 타입 정의

--- client/src/features/main/QuestionSection.tsx
+++ client/src/features/main/QuestionSection.tsx
@@ -109,7 +109,7 @@ export default function QuestionSection() {
 
   const sentenceAnimations = [sentenceAnimation1, sentenceAnimation2, sentenceAnimation3];
 
-  const dots = useCallback(
+  const dots: () => JSX.Element[] = useCallback(
     () =>
       [0, 1, 2].map((i) => (
         <Dot
@@ -126,7 +126,7 @@ export default function QuestionSection() {
     [dotAnimation.controls]
   );
 
-  const sentences = useCallback(
+  const sentences: () => JSX.Element[] = useCallback(
     () =>
       lines.map((line, i) => {
         const { ref, controls } = sentenceAnimations[i]; // 각 문장마다 ref & controls 사용
