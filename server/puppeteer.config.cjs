3f8eb81013e5995d320749a3507eb90852d26efd
hyonun321 <hyo_nun@kakao.com>
2025-05-19 14:28:31 +0900

    Buile: 캐시디렉토리 제거

--- server/puppeteer.config.cjs
+++ server/puppeteer.config.cjs
@@ -4,7 +4,6 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
   chrome: {
     skipDownload: false,
   },
