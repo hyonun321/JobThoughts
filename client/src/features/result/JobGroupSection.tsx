1c1cfb2cf4fe4e38a5cd1393527aa5b075db12dc
sejinbaek <sejin9767@gmail.com>
2025-05-20 11:18:44 +0900

    Style: 직업 리스트 제목 부분 자연스러운 반응형 처리
    - 이미지에 z-index를 줘서 h2가 이미지쪽으로 넘어갈 수 있도록 처리
    - h2를 따로 컨테이너로 감싸서 margin-right를 음수값으로 줘서 넘어갈 수 있도록 처리
    - 폰트 사이즈, 간격 등 전반적인 사이즈 조정
    -

--- client/src/features/result/JobGroupSection.tsx
+++ client/src/features/result/JobGroupSection.tsx
@@ -33,43 +33,54 @@ const Section = styled.div`
 `;
 
 const TitleContainer = styled.div`
+  position: relative;
   display: flex;
   justify-content: space-between;
+  align-items: flex-end;
 `;
 
 // 제목 텍스트 부분
 const TextArea = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
+
+  h3 {
+    font-size: clamp(${theme.fontSize.xs}, 2vw, ${theme.fontSize.lg});
+    margin: 0 0 clamp(10px, 5vw, 30px) 0;
+  }
+`;
+
+const H2Wrapper = styled.div`
+  margin-right: -130px;
+  white-space: nowrap;
 
   h2 {
-    font-size: clamp(${theme.fontSize.m}, 3vw, ${theme.fontSize.xl});
-    margin: clamp(5px, 1vw, 10px) 0;
+    font-size: clamp(${theme.fontSize.xs}, 3vw, ${theme.fontSize.xl});
+    margin: 0;
   }
 
   h2 span {
     color: ${theme.colors.primary};
   }
-
-  h3 {
-    font-size: clamp(${theme.fontSize.s}, 2vw, ${theme.fontSize.lg});
-    margin: 0 0 clamp(10px, 5vw, 30px) 0;
-  }
 `;
 
 const ImageArea = styled.div`
+  position: relative;
+  z-index: 10;
   display: flex;
   align-items: flex-end;
+  height: clamp(100px, 20vw, 200px);
 
   img {
-    width: clamp(150px, 40vw, 350px);
+    width: clamp(130px, 37vw, 350px);
     height: auto;
     object-fit: contain;
+    z-index: 10;
   }
 `;
 
-const TopTitle = styled.span`
+const TopTitle = styled.div`
   width: fit-content;
   margin-bottom: clamp(12px, 2vw, 20px);
   background-color: #f5f9ff;
@@ -77,7 +88,7 @@ const TopTitle = styled.span`
   padding: 8px 16px;
   border-radius: 100px;
   box-shadow: 2px 3px 1px rgba(200, 224, 255, 1);
-  font-size: clamp(${theme.fontSize.xs}, 2vw, ${theme.fontSize.lg});
+  font-size: clamp(${theme.fontSize.xxs}, 2vw, ${theme.fontSize.lg});
   font-weight: ${theme.fontWeight.medium};
 `;
 
@@ -85,7 +96,7 @@ const TopTitle = styled.span`
 const JobListContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: clamp(8px, 3vw, 32px);
+  gap: clamp(16px, 3vw, 32px);
 `;
 
 // 계열별 직업 리스트 레이아웃 O
@@ -119,7 +130,7 @@ const Category = styled.div`
   }
 
   h4 {
-    font-size: clamp(${theme.fontSize.s}, 1.5vw, ${theme.fontSize.lg});
+    font-size: clamp(${theme.fontSize.xs}, 1.5vw, ${theme.fontSize.lg});
     margin-left: -5px;
     white-space: wrap;
   }
@@ -169,13 +180,16 @@ export default function JobGroupSection({ topValues, jobsByMajor }: ResultJobLis
 
   return (
     <Section>
+      <TopTitle>나의 가치관과 관련이 높은 직업</TopTitle>
       <TitleContainer>
         <TextArea>
-          <TopTitle>나의 가치관과 관련이 높은 직업</TopTitle>
-          <h2 style={{ padding: '0px', margin: '0px' }}>
-            <span>{topValues[0]}</span>도 챙기고, <span>{topValues[1]}</span>도 놓치기 싫은 당신!
-          </h2>
-          <h2 style={{ padding: '0px', margin: '0px' }}>이런 직업은 어때요?</h2>
+          <H2Wrapper>
+            <h2 style={{ padding: '0px', margin: '0px' }}>
+              <span>{topValues[0]}</span>도 챙기고, <span>{topValues[1]}</span>도 놓치기 싫은 당신!
+            </h2>
+            <h2 style={{ padding: '0px', margin: '0px' }}>이런 직업은 어때요?</h2>
+          </H2Wrapper>
+
           <h3 style={{ marginTop: '10px', color: '#212121' }}>
             직업을 클릭하면, 실시간 채용 공고까지 확인할 수 있어요.
           </h3>
